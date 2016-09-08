293a04494fd59916328654eed05f0860ed4ca4dd
ana <syuzyuzyu@gmail.com>
2016-09-08 11:29:49 +0000

    repair point of reviewer

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true
+  validates :profile, length: { maximum: 140 }
+  validates :place, length: { maximum: 50 }
   has_secure_password
 end
