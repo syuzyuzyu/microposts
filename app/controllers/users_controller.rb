31333266b73bad2610e371d26c6b4390536e635f
ana <syuzyuzyu@gmail.com>
2016-09-11 02:14:19 +0000

    add kaminari at profile page because reviewer point out it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   
   def show # 追加
     @user = User.find(params[:id])
-    @microposts = @user.microposts.order(created_at: :desc)
+    @microposts = @user.microposts.order(created_at: :desc).page(params[:page]).per(5)
   end
   
   def new
